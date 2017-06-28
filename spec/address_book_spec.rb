9b3541ecbbcd8b46e6ba467bbc8f78e62c0afbfd
Nick Pratt <nicholasapratt@gmail.com>
2017-06-28 14:29:53 +0000

    assignment 6: completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -86,7 +86,34 @@ RSpec.describe AddressBook do
            entry_five = book.entries[4]
            check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
          end
-       end
+     end
+       
+    describe "import from entries_2.csv" do 
+        
+        it "imports correct numbe rof entries" do 
+            book.import_from_csv("entries_2.csv")
+            book_size = book.entries.size
+            expect(book_size).to eq 3
+        end
+        
+        it "imports entry one" do 
+            book.import_from_csv("entries_2.csv")
+            entry_one = book.entries[0]
+            check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+         end
+         
+         it "imports entry two" do 
+            book.import_from_csv("entries_2.csv")
+            entry_two = book.entries[1]
+            check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+         end
+         
+         it "imports entry three" do 
+            book.import_from_csv("entries_2.csv")
+            entry_three = book.entries[2]
+            check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+         end
+     end
     
     describe "#remove_entry" do 
         let(:book) { AddressBook.new }
